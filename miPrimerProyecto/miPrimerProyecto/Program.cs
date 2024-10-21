5b3d3263792e61829b1968a84dc5386a0465f620
Maria Hernandez <maria.hdez07@outlook.com>
2024-10-20 21:53:11 -0600

    Creacion de mi primer proyecto

---
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, chicos de programacion computacional 1, Bienvenidos...");
+
+            Console.ReadLine(); // Para que no se cierre la consola
+        }
+    }
+}
